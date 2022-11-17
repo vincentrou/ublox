8655a6be8ed6c96cbe3e6c9f15140312cec2debe
Vincent Rousseau <vincent.rousseau@sabi-agri.com>
2022-11-17 16:21:28 +0100

    increase timeout

--- ublox_gps/include/ublox_gps/gps.hpp
+++ ublox_gps/include/ublox_gps/gps.hpp
@@ -69,7 +69,7 @@ class Gps final {
   //! Sleep time [ms] after setting the baudrate
   const int kSetBaudrateSleepMs = 500;
   //! Default timeout for ACK messages in seconds
-  constexpr static double kDefaultAckTimeout = 1.0;
+  constexpr static double kDefaultAckTimeout = 10.0;
   //! Size of write buffer for output messages
   constexpr static int kWriterSize = 2056;
 
